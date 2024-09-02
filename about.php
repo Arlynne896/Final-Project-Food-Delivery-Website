99c87b68c38e2fbe24b8966ed9e881730450a9aa
Arlynne Abalain <arlynneabalain2004@gmail.com>
2024-09-02 20:04:24 +0800

    Initial Commit

---
+++ about.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		
+		<title>About Us</title>
+		<link href="sample.css" rel="stylesheet"/>
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	</head>
+	<body>
+		<nav>
+			<div class="navbar">
+               <class="nav"><a href="homepage.php">Home</a>
+               <class="nav"><a href="about.php">About Us</a>
+			   <class="nav"><a href="shop.php">Partnerships</a>
+			   <class="nav"><a href="faq.php">Frequently Asked</a>
+			   <class="nav"><a href="contact.php">Contact Us</a>
+			</div>
+		</nav>
+		<div class="header">
+			<h1>About Us<br></h1><h3>The moment you tap, we’re already there</h3>
+		</div>
+		<main>
+			<article>
+				<p>With <b>[Website Name]</b> at your service, just one tap away and we’ve already zapped/arrived/teleported right at your doorstep! Too fast for you to react. We’re not just a food delivery service; 
+				we're your culinary teleportation hub. Our mission is to make sure that you never have to wait too long for a delicious meal. Whether you’re craving comfort food, exploring exotic cuisines, or just need a 
+				quick snack, we’ve got you covered. </p>
+			</article>
+		
+			<section>
+				<h2>Our Story</h2>
+				<p>Born out of a love for a quick delicious meal and a passion for technology, [Website Name] was created to bridge the gap between you and the your cravings. 
+				Life can be hectic, cooking and going out to eat can be tedious, and we believe that your meal shouldn’t add to the stress. That’s why our platform delivers meals 
+				at the speed of light, ensuring your food arrives with teleportation-like precision, preserving its quality and freshness as if it was just served. </p>
+			</section>
+		
+			<section>
+				<h2>Our Promise</h2>
+				<ul>
+					<li><b>Speed</b>: Our delivery system is like a teleportation device for your taste buds. We get your food to you at warp speed, hot and fresh, every single time.</li>
+					<li><b>Tech-Savvy Tastes</b>: From local legends to global sensations, our tech-powered platform brings an array of culinary delights straight to your door. The world of flavors is just a click away.</li>
+					<li><b>Simplicity</b>: Our platform is your digital chef’s assistant. A few taps, and your meal is virtually teleported to your table—no fuss, no hassle!</li>
+				</ul>
+			</section>
+		
+			<section>
+				<h2>Why us?</h2>
+				<ul>
+					<li><b>Instant Gratification</b>: No more waiting in long lines or dealing with traffic. We teleport your meal from the restaurant to your table in no time.</li>
+					<li><b>Tech-Savvy Tastes</b>: From local legends to global sensations, our tech-powered platform brings an array of culinary delights straight to your door. The world of flavors is just a click away.</li>
+					<li><b>Simplicity</b>: Our platform is your digital chef’s assistant. A few taps, and your meal is virtually teleported to your table—no fuss, no hassle!</li>
+				</ul>
+			</section>
+		
+			<span>
+				<p>Join us in revolutionizing the way you experience food. Whether you're at home, at work, or on the go, [Website Name] is here to satisfy your hunger—instantly.</p>
+			</span>
+		</main>
+		<hr>
+		<footer>
+			<p style="margin-left:30px;"><small>Made by Abalain, Almaida, Catimbang, Cuano & Morante 2024 © Copyright Intended</small></p>
+		</footer>
+	</body>
+</html>
