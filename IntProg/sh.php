5891d6ce1bdcd977adeb912b2fd8e09c54657a19
AngelicaAlmaida04 <angelicaalmaida2004@gmail.com>
2024-11-28 10:58:41 +0800

    Add files via upload

---
+++ IntProg/sh.php
@@ -0,0 +1,104 @@
+
+
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>HOME</title>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <script src='https://kit.fontawesome.com/a076d05399.js' crossorigin='anonymous'></script>
+        <script src="https://kit.fontawesome.com/8ad836cb28.js" crossorigin="anonymous"></script>
+        <link rel="stylesheet" href="css.css">
+    </head>
+    <body>
+       <nav>
+          <div class="navbar">
+            <img src="image/logo.jpg" class="logo" alt="logo">
+           <ul> 
+              <li><a href="#section1">   <i class="fas fa-home"></i>   </li>
+              <li><a href="#section2">    <i class="fas fa-shopping-cart"></i></li>
+              <li><a href="#section3">  <i class='fas fa-user-friends'></i></li>
+             <li><a href="#section4">  hasbj</li>
+         </ul>
+        </nav>
+    
+      <div class="content">
+          <section id="section1">
+               <div class="cartIcon">
+                   <p class="mycart">Order Now....</p>
+                        <div class ="cartIcon2">
+                            <i class="fa fa-shopping-cart"></i>
+                            <p id="count" >0</p>        
+                        </div>
+                </div> 
+                <div class="sec1">
+                      <img src="image/front.jpg" class="menu" alt="menuSushi">
+                     <h3>SOMETIMES, ALL YOU NEED IS SUSHI!</h3><br>
+               </div> 
+          </section>
+          <div class="scroll">
+           <section id="section2">
+                <div class="span">
+                      <div class="seccon">
+                          <div class="sidebar">
+                             <div class="start"><p>My Cart</p></div>
+                                  <div id="cartItem"> 
+                                      <div class="space">
+                                         <p>Your cart is empty</p>
+                                     </div>
+                                  </div>
+                                  <div class="foot">
+                                         <h3 >Total</h3>
+                                         <h2 id="total">₱ 0.00</h2>
+                                  </div>
+                            </div>
+                            <div class="address">    
+                                 <div id="root">
+        
+                                </div>
+                            </div>
+                       </div>
+               </div>
+            </section>
+         </div>
+            <section id="section3">
+                <div class="about">
+                     <h2>About Us</h2>
+                     <img src="image/kitchen.jpg" class="kitchen" alt="BusyKitchen">
+                     <h3>Sushi is the best for everyone's choice!</h3><br>
+                     <p style="padding-left:15%; padding-right:9%;font-size: 23px; ">Hey, everything you need is in our store! So what are you guys waiting for? Issho ni tabemasu yo!<br> <h4>私たちの寿司は美味しいて、食べましょ！</h4>
+                     </p>
+                     <h3 style="padding-left:35%; font-size: 47px;">Our Sushi store</h3>
+                     <p style="padding-left:12%;padding-right:9%;font-size: 24px;"> In the world full of stores, our team is here to serve you these different kinds of special sushi that everyone's</p><br>
+                     <p style="padding-left:9%;padding-right:9%;font-size: 24px;">looking for, the best sushi that you've ever taste in your whole life, its texture, freshnness and of course </p><br><p style="padding-left:9%;font-size: 24px; color: rgb(203, 140, 58);">its taste.</p><br>
+                     <p style="padding-left:9%;padding-right:9%;font-size: 24px;">To take your order, you can go directly to our place nearby or you can just log in to our website, place your order </p><br>
+                     <p style="padding-left:9%;padding-right:9%;font-size: 24px;">and wait for your delivery! </p>  
+                     <h3 style="padding-left:38%; font-size: 47px;">Our mission</h3>
+                     <p style="padding-left:12%;padding-right:9%;font-size: 24px;">Our mission is to maintain and support the services that our store offers to every customers. By the use of </p><br>
+                     <p style="padding-left:9%;padding-right:9%;font-size: 24px;"> this website we can provide and deliver the foods chose or placed by our customers. </p><br>               
+                     <h3 style="padding-left:38%; font-size: 47px;">Our vision</h3>
+                     <p style="padding-left:12%;padding-right:9%;font-size: 24px;"> Our vision is to improve our skills, services and to become professional experts that will also serve as a big</p><br>
+                     <p style="padding-left:9%;padding-right:9%;font-size: 24px;"> help in our team to enlighten the skills and knowledge of those individuals that want to take incharge in our  </p><br>
+                     <p style="padding-left:9%;padding-right:9%;font-size: 24px;">positions in the near future. </p>
+                </div>
+            </section>
+
+            <script src="jjjj.js"></script>
+           <footer>
+		    	<p style="margine-left:34%; color: antiquewhite;"><small>Made by Abalain, Almaida, Catimbang, Cuano & Morante 2024 © Copyright Intended</small></p>
+		      </footer>
+          
+    </body>
+</html>
+<?php 
+/*}else 
+{
+    header("Location:mainpage.php");
+    exit();
+
+}
+
+
+?>*/
+
